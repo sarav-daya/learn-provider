04040958c19c3548ff8dd0ef132c4f48f9c78c93
Saravanan Dayalan <saravanan.dayalan@aveva.com>
2022-01-20 09:01:24 +0530

    Counter App without provider

--- lib/main.dart
+++ lib/main.dart
@@ -10,30 +10,29 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Counter App',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int counter = 0;
 
-  void _incrementCounter() {
+  void increment() {
     setState(() {
-      _counter++;
+      counter++;
     });
   }
 
@@ -41,26 +40,124 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: const Text('My Counter'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Container(
+              color: Colors.blue[100],
+              padding: const EdgeInsets.all(20.0),
+              child: const Text(
+                'MyHomePage',
+                style: TextStyle(fontSize: 24.0),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            const SizedBox(height: 20.0),
+            CounterA(
+              counter: counter,
+              increment: increment,
             ),
+            const SizedBox(height: 20.0),
+            Middle(counter: counter),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+    );
+  }
+}
+
+class CounterA extends StatelessWidget {
+  final int counter;
+  final void Function() increment;
+
+  const CounterA({
+    Key? key,
+    required this.counter,
+    required this.increment,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red[100],
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        children: [
+          Text(
+            '$counter',
+            style: const TextStyle(fontSize: 48.0),
+          ),
+          ElevatedButton(
+            onPressed: increment,
+            child: const Text(
+              'Increment',
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Middle extends StatelessWidget {
+  final int counter;
+  const Middle({
+    Key? key,
+    required this.counter,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.grey[200],
+      padding: const EdgeInsets.all(20.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          CounterB(counter: counter),
+          const SizedBox(width: 20.0),
+          const Sibling(),
+        ],
+      ),
+    );
+  }
+}
+
+class CounterB extends StatelessWidget {
+  final int counter;
+  const CounterB({
+    Key? key,
+    required this.counter,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.yellow[100],
+      padding: const EdgeInsets.all(10.0),
+      child: Text(
+        '$counter',
+        style: const TextStyle(fontSize: 24.0),
+      ),
+    );
+  }
+}
+
+class Sibling extends StatelessWidget {
+  const Sibling({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.orange[100],
+      padding: const EdgeInsets.all(10.0),
+      child: const Text(
+        'Sibling',
+        style: TextStyle(fontSize: 24.0),
       ),
     );
   }
