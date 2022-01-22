4b82010d4b505a0ee496ebd8b7e9ecc4ee060dc0
Saravanan Dayalan <dsaravanan.che@gmail.com>
2022-01-22 14:39:18 +0530

    Why ProxyProvider?

--- lib/main.dart
+++ lib/main.dart
@@ -1,93 +1,105 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:provider_tutorials/models/counter.dart';
-import 'package:provider_tutorials/show_me_counter.dart';
+import 'package:provider_tutorials/pages/Proxyprov_update.dart';
+import 'package:provider_tutorials/pages/why_proxyprovider.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  MyApp({Key? key}) : super(key: key);
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  final _counter = Counter();
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Named Route Example',
+      title: 'ProxyProvider Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      onGenerateRoute: (RouteSettings settings) {
-        switch (settings.name) {
-          case '/':
-            return MaterialPageRoute(
-              builder: (context) => ChangeNotifierProvider.value(
-                value: _counter,
-                child: MyHomePage(),
-              ),
-            );
-          case '/counter':
-            return MaterialPageRoute(
-              builder: (context) => ChangeNotifierProvider.value(
-                value: _counter,
-                child: ShowMeCounter(),
-              ),
-            );
-          default:
-            return null;
-        }
-      },
+      home: const HomePage(),
     );
-  }
-
-  @override
-  void dispose() {
-    _counter.dispose();
-    super.dispose();
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Named Route Example'),
+        title: Text('ProxyProvider Demo'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              child: Text(
-                'Show Me Counter',
-                style: TextStyle(fontSize: 20.0),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30.0),
+          child: ListView(
+            shrinkWrap: true,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => WhyProxyProv()));
+                },
+                child: Text(
+                  'Why\nProxyProvider',
+                  style: TextStyle(fontSize: 20.0),
+                  textAlign: TextAlign.center,
+                ),
               ),
-              onPressed: () {
-                Navigator.pushNamed(context, '/counter');
-              },
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            ElevatedButton(
-              onPressed: context.read<Counter>().increment,
-              child: Text(
-                'Increment Counter',
-                style: TextStyle(fontSize: 20.0),
+              SizedBox(height: 10.0),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => ProxyProvUpdate()));
+                },
+                child: Text(
+                  'ProxyProvider\nupdate',
+                  style: TextStyle(fontSize: 20.0),
+                  textAlign: TextAlign.center,
+                ),
               ),
-            )
-          ],
+              SizedBox(height: 10.0),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text(
+                  'ProxyProvider\ncreate/update',
+                  style: TextStyle(fontSize: 20.0),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              SizedBox(height: 10.0),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text(
+                  'ProxyProvider\nProxyProvider',
+                  style: TextStyle(fontSize: 20.0),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              SizedBox(height: 10.0),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text(
+                  'ChangeNotifierProvider\nChangeNotifierProxyProvider',
+                  style: TextStyle(fontSize: 20.0),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              SizedBox(height: 10.0),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text(
+                  'ChangeNotifierProvider\nProxyProvider',
+                  style: TextStyle(fontSize: 20.0),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
